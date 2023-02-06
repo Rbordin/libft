914460d102c4fd3f9cd4c74fd8b3ce05c492ac88
Rbordin <123752418+Rbordin@users.noreply.github.com>
2023-02-06 09:33:05 +0100

    Add files via upload

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,4 +12,19 @@
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *node;
 
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		node = ft_lstnew(f(lst->content));
+	if (!node)
+	{
+		ft_lstclear(&lst, del);
+		return (NULL);
+	}
+	if (lst->next == NULL)
+		return (node);
+	return (NULL);
+}
